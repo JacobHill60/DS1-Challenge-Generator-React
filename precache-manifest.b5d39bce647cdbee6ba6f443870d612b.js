212d30014af2de1314d4b6f0f071d40a146390c1
JacobHill60 <jacobdhill60@gmail.com>
2020-05-13 17:39:23 -0400

    Updates

---
+++ precache-manifest.b5d39bce647cdbee6ba6f443870d612b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b72cad0a1e796ee5c7f3321e7b1f371",
+    "url": "/DS1-Challenge-Generator-React/index.html"
+  },
+  {
+    "revision": "e1b8eeeed4058ed30643",
+    "url": "/DS1-Challenge-Generator-React/static/css/main.fb2ef231.chunk.css"
+  },
+  {
+    "revision": "e117752c02aaac2aff1d",
+    "url": "/DS1-Challenge-Generator-React/static/js/2.c24d0e8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DS1-Challenge-Generator-React/static/js/2.c24d0e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b8eeeed4058ed30643",
+    "url": "/DS1-Challenge-Generator-React/static/js/main.feaffa0d.chunk.js"
+  },
+  {
+    "revision": "653a89a4efad6562fe0f",
+    "url": "/DS1-Challenge-Generator-React/static/js/runtime-main.08de0888.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DS1-Challenge-Generator-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
